8255e8d41cfbb611694632ff01eb589480aa3a82
goncaloRM <goncalo.ribeiro.mendes7@gmail.com>
2023-09-15 10:33:20 -0400

    "Created main that runs fizzbuzz"

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] EMPTY_ARRAY = new String[0];
+        FizzBuzz.main(EMPTY_ARRAY);
+        FizzBuzz.doFizzBuzz(5);
+    }
+}
